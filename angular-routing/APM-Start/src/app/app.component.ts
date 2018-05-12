89ee3eebb05e68def0750c68505a2d5966d17573
Michael <mike.alex.martinez@gmail.com>
2018-05-12 22:52:10 +0100

    reading route parameters with observables

---
+++ angular-routing/APM-Start/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './user/auth.service';
+
+@Component({
+    selector: 'pm-app',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+
+    constructor(private authService: AuthService,
+                private router: Router) { }
+
+    logOut(): void {
+        this.authService.logout();
+        console.log('Logged out');
+        // ensure other parameters are removed when user logs out
+        this.router.navigateByUrl('/welcome');
+    }
+}
